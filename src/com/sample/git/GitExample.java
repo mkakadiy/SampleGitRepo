32414b2dc3ca93898ca97ce1aedc4b474c4dc298
Mitesh <kakadiamitesh@gmail.com>
2018-12-25 15:30:13 +0530

    initial commit

---
+++ src/com/sample/git/GitExample.java
@@ -0,0 +1,11 @@
+package com.sample.git;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println(GitExample.class.getName());
+
+	}
+
+}
---
+++ src/com/sample/git/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sample.git;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println(HelloWorld.class.getName());
+	}
+
+}
